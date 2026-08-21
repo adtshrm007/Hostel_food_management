0353616f942765408eac77d12a0ae0981e7cd883
Skit-025 <codesheritage@gmail.com>
2026-08-22 01:31:11 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -14,7 +14,7 @@ axiosInstance.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    return ;
+    return config;
   },
   (error) => Promise.reject(error)
 );
